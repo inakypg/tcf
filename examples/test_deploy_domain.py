cf348134bd3257c5e2f5a433d3ca29a256f166f7
Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
2018-10-11 14:02:20 -0700

    examples/test_deploy_domain: template for a TC that uses boot_domain
    Signed-off-by: Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>

--- examples/test_deploy_domain.py
+++ examples/test_deploy_domain.py
@@ -1,56 +1,65 @@
 #! /usr/bin/python
+#
+# Add to your config:
+#
+# tcfl.config.url_add('https://jfsotc09.jf.intel.com:5001',
+#                    ssl_ignore = True)
+#
+#
+# $ [DOMAIN_VERSION=91] tcf run -v test_deploy_domain.py
+
 import os
+import re
 
 import tcfl.tc
+import tcfl.tl
 
-domain = os.environ["DOMAIN"]
-domain_part = os.environ["DOMAIN_PART"]
-domain_seed = os.environ.get("DOMAIN_SEED", None)
+domain = os.environ.get("DOMAIN", "Fedora")
+version = os.environ.get("DOMAIN_VERSION", 28)
 
-@tcfl.tc.interconnect()
-@tcfl.tc.target()
-class deploy_domain(tcfl.tc.tc_c):
+@tcfl.tc.interconnect("ipv4_addr")
+@tcfl.tc.target('linux and boot_interconnect')
+class aio(tcfl.tc.tc_c):
 
     # FIXME move to deploy phase; need to fix things in tc.py so we
     # can use the expecter
-    
-    def eval(self, target, ic):
-        
-        target.report_info("rebooting into service domain for flashing")
-        target.property_set("boot_domain", "service")
+    def eval_10_deploy(self, target, ic):
+
+        ic.power.cycle()
+        # Deploy
+        tcfl.tl.domain_deploy(ic, target, domain, "%s-%d" % (domain, version),
+                              "/dev/sda", "/dev/sda5")
+
+        # If there are errors, exceptions will come,but otherwise we
+        # are here, still in the service OS, so reboot into our new OS
         target.power.cycle()
+
+        # FIXME: HACK -- this should happen after a power cycle?
+        # should be a post hack installed by expecters
+        # FIXME: do for each console
+        tcfl.expecter.console_rx_flush(self.expecter, target, truncate = True)
+
+
+        # Wait for the OS to boot, login as root in the serial console
+        # FIXME This is kinda distro specific and needs to be streamlined
+        target.expect(re.compile('login:'))	# wait for "i booted prompt"
+        target.send('root')			# login as root, assumes passwordless
+        # now that we logged in, change the regex expectation
+        target.shell.linux_shell_prompt_regex = re.compile('root@.*# ')
+        # and make sure the targe this up and the shell properly configured
         target.shell.up()
 
-        # Trap the shell to complain loud if a command fails, and catch it
-        target.send("trap 'echo ERROR-IN-SHELL' ERR")
-        target.on_console_rx("ERROR-IN-SHELL", result = 'fail', timeout = False)
+        # We don't need the interconnect anymore by-- after we booted!!
+        # release it for anyone else -- a TC that needs the
+        # interconnect would not do this
+        ic.release()
+        target.report_pass("fedora-%d: booted" % version)
 
-        rsync_server = ic.kws['ipv4_addr']
-        
-        # FIXME: recover if not formated
-        try:
-            target.report_info("mounting /boot and /mnt")
-            # FIXME: act on
-            # [   15.500607] FAT-fs (vdd1): Volume was not properly unmounted. Some data may be corrupt. Please run fsck.
-            target.shell.run("mount /dev/disk/by-partlabel/tcf-boot /boot")
-            # FIXME: act on failing, just reformat and retry, then
-            # bail out on failure
-            target.shell.run("mount /dev/disk/by-partlabel/tcf-root-%s /mnt"
-                             % domain_part)
-            if domain_seed:
-                target.report_info("rsyncing seed %s to /mnt" % domain_seed)
-                target.shell.run(
-                    "time rsync -a --delete %s::images/%s/. /mnt/."
-                    % (rsync_server, domain))
-                # fugly, need way better semantics -- paramiko? pypy?
-                # however, this runs contrary to clear steps that the
-                # user can reproduce typing
-                target.shell.run("[ -r /mnt/boot/bzImage ] && cp -f /mnt/boot/bzImage /boot/vmlinuz-%s" % domain)
-                target.shell.run("[ -r /mnt/boot/vmlinuz ] && cp -f /mnt/boot/bzImage /boot/vmlinuz-%s" % domain)
-                target.shell.run("[ -r /mnt/boot/initramfs ] && cp -f /mnt/boot/initramfs /boot/initramfs-%s" % domain)
-            console_so_far = target.console.read()
-            target.property_set("boot_domain", domain)
-        finally:
-            target.shell.run("umount /dev/disk/by-partlabel/tcf-boot")
-            target.shell.run("umount /dev/disk/by-partlabel/tcf-root-%s"
-                             % domain_part)
+    #
+    # Run our tests
+    #
+    def eval_20(self, target):
+        # run the command that launches the tests, wait for the shell
+        # prompt; if it fails, this will raise an exception
+        # FIXME: we need hooks to detect kernel panics, oopses, etc
+        target.shell.run("echo I booted", "I booted")
