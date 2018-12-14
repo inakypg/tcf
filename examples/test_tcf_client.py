1ede97aaa2c2407e825a698180b86d91acbbd08c
Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
2018-12-13 17:50:33 -0800

    WIP: examples/test_tcf_client: rearrange
    Signed-off-by: Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>

--- examples/test_tcf_client.py
+++ examples/test_tcf_client.py
@@ -12,15 +12,13 @@ import pprint
 
 @tcfl.tc.interconnect("ipv4_addr")
 @tcfl.tc.target('pos_capable')
-class _test(tcfl.tc.tc_c):
+class _base(tcfl.tc.tc_c):
             
     def deploy_10(self, ic, target):
-
         ic.power.cycle()
-        tcfl.pos.deploy_image(ic, target, "fedora")
+        tcfl.pos.deploy_image(ic, target, self.image)
 
-    def start_10(self, target):
-        
+    def start_10(self, target):        
         # If there are errors, exceptions will come,but otherwise we
         # are here, still in the service OS, so reboot into our new OS
         target.power.cycle()
@@ -30,13 +28,29 @@ class _test(tcfl.tc.tc_c):
         target.report_pass("booted")
         self._targets_active()
 
+
     def eval_10_setup(self, ic, target):
+        """
+        Sets the target up:
+        
+        - export a proxy if one needed in /etc/bashrc
+        - install python-pip2
+        - add a testuser, login into it
+        """
         if 'http_proxy' in ic.kws:
-            target.shell.run("export http_proxy=%s" % ic.kws.get('http_proxy'))
-            target.shell.run("export HTTP_PROXY=%s" % ic.kws.get('http_proxy'))
+            target.shell.run("""\
+cat >> /etc/bashrc <<EOF
+export http_proxy=%s
+export HTTP_PROXY=%s
+""" % ic.kws.get('http_proxy'), ic.kws.get('http_proxy'))
+
         if 'https_proxy' in ic.kws:
-            target.shell.run("export https_proxy=%s" % ic.kws.get('https_proxy'))
-            target.shell.run("export HTTPS_PROXY=%s" % ic.kws.get('https_proxy'))
+            target.shell.run("""\
+cat >> /etc/bashrc <<EOF
+export https_proxy=%s
+export HTTPS_PROXY=%s
+""" % ic.kws.get('https_proxy'), ic.kws.get('https_proxy'))
+
         self.tls.expecter.timeout = 120
         target.shell.run("dnf install -y python2-pip")
         self.tls.expecter.timeout = 30
@@ -45,16 +59,14 @@ class _test(tcfl.tc.tc_c):
         target.shell.linux_shell_prompt_regex = re.compile(r'testuser@.*\$ ')
         target.shell.run("su - testuser")
         target.shell.up()
-
-        if 'http_proxy' in ic.kws:
-            target.shell.run("export http_proxy=%s" % ic.kws.get('http_proxy'))
-            target.shell.run("export HTTP_PROXY=%s" % ic.kws.get('http_proxy'))
-        if 'https_proxy' in ic.kws:
-            target.shell.run("export https_proxy=%s" % ic.kws.get('https_proxy'))
-            target.shell.run("export HTTPS_PROXY=%s" % ic.kws.get('https_proxy'))
+        # Note the proxy setting in /etc/bashrc is now picked up automagically
 
 
-    def eval_20_install(self, ic, target):
+    def server_install(self, ic, target):
+        """
+        Install the server following the same steps as the
+        :ref:`server installation guide <install_tcf_server_rpms>`
+        """
         target.shell.run("git clone http://github.com/intel/tcf tcf.git")
         target.shell.run("cd tcf.git")
         git_version = target.shell.run("git describe", output = True)
@@ -69,8 +81,30 @@ class _test(tcfl.tc.tc_c):
         target.shell.run("python2 setup.py install --user")
         target.shell.run("cd zephyr")
         target.shell.run("python2 setup.py install --user")
+
+
+    def client_quickstart_install(self, ic, target):
+        """
+        Install the client following the same  steps as the
+        :ref:`quickstart <install_tcf_client>`
+        """
+        target.shell.run("git clone http://github.com/intel/tcf tcf.git")
+        target.shell.run("cd tcf.git")
+        git_version = target.shell.run("git describe", output = True)
+        # this comes as:
+        # git describe
+        # v0.11-69-g9900ff2
+        # [testuser@localhost tcf.git]$ 
+        self.git_version = git_version.split('\n')[1].strip()
+        self.report_info("git version: %s" % self.git_version)
+
+        target.shell.run("pip2 install --user -r requirements.txt")
+        target.shell.run("python2 setup.py install --user")
+        target.shell.run("cd zephyr")
+        target.shell.run("python2 setup.py install --user")
+
         
-    def eval_30_bat(self, ic, target):
+    def client_bat(self, ic, target):
         target.shell.run("cd")
         tcf_version = target.shell.run("tcf --version", output = True)
         # this comes as:
@@ -84,5 +118,16 @@ class _test(tcfl.tc.tc_c):
                 "checked out version (%s)" % (self.git_version, tcf_version))
         target.shell.run("tcf --help")
         target.shell.run("tcf list")
+
+
     def teardown(self):
         tcfl.tl.console_dump_on_failure(self)
+
+
+class _fedora_client_install(_base):
+
+    image = "fedora"            
+        
+    def eval_30_client_bat(self, ic, target):
+        self.client_quickstart_install(ic, target)
+        self.client_bat(ic, target)
